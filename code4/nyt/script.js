dc0a6e01d2da0159875d1edec39ae9886ebcdcbf
xonatis-academy <formation@xonatis.com>
2020-12-24 17:05:57 +0100

    NYT: Adding jquery

---
+++ code4/nyt/script.js
@@ -0,0 +1,30 @@
+$(document).ready(
+
+    function () {
+        let taux = 0.8;
+
+        function titi() {
+            taux = taux + 0.1;
+            
+            $('#nasdaq-value').text(taux);
+
+            setTimeout(function () {
+                titi();
+            }, 1000);
+        }
+
+        titi();
+
+        $('.lien-inter').click(
+            
+            function() {
+
+                $('.book-to-change').text("Le Podcast de l'Amour Moderne");
+                $('.book-to-change').addClass('text-en-rouge');
+                
+            }
+        
+        );
+    }
+
+);
